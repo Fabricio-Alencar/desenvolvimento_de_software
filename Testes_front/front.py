a48a7206650dbecaea32a621477f76ddf573c893
Fabricio <10428537@mackenzista.com.br>
2025-03-06 10:20:04 -0300

    front.py

--- Testes_front/front.py
+++ Testes_front/front.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 
 dados = []
@@ -26,22 +27,27 @@ def voltar_pagina1():
 
 
 def Cadastrar_Administrativo():
-    nome = nomeADM.get()  # Pega o valor da entrada "Nome"
-    cpf = cpfADM.get()  # Pega o valor da entrada "Idade"
-    
-    # Aqui você pode armazenar os dados em uma lista ou base de dados, por exemplo:
-    dados.append((nome, cpf))  # Armazenando os dados em uma lista
-    
+    funcionario = [] 
+    funcionario.append((
+        nomeADM.get(),        # Obtendo o nome
+        cpfADM.get(),         # Obtendo o CPF
+        telefoneADM.get(),    # Obtendo o telefone
+        horarioADM.get(),     # Obtendo o horário
+        setorADM.get(),       # Obtendo o setor
+        nivelADM.get()        # Obtendo o nível
+    ))
+    dados.append(funcionario)  # Adicionando o funcionário à lista de dados
     # Exibindo uma mensagem ou algum feedback
-    print(f"Cadastro realizado: {nome}, {cpf}")
+    print(f"Cadastro realizado: {nomeADM.get()}, {cpfADM.get()}, {telefoneADM.get()}, {horarioADM.get()}, {setorADM.get()}, {nivelADM.get()}")
     
-    voltar_pagina1()  # Depois de cadastrar, volta para a página 1
+    voltar_pagina1()  # Função para voltar para a página 1 após o cadastro
+
 
 
 # Criando a janela principal
 root = tk.Tk()
 root.title('Cadastro de Funcionários')
-root.geometry('400x400') # Horizontal x Vertical
+root.geometry('400x600') # Horizontal x Vertical
 
 
 
@@ -78,7 +84,7 @@ button.pack(pady=20)
 pagina2 = tk.Frame(root)  # Segunda página (frame)
 
 # Label
-label2 = tk.Label(pagina2, text="Esta é a Página 2", font=("Arial", 16))
+label2 = tk.Label(pagina2, text="Funcionário Administrativo", font=("Arial", 16))
 label2.pack(pady=20)  # Usando pack para o label com espaçamento
 
 # Labels e campos de entrada
@@ -86,15 +92,32 @@ tk.Label(pagina2, text='Nome').pack()  # Label para "Nome"
 nomeADM = tk.Entry(pagina2)  # Primeira entrada
 nomeADM.pack(pady=5)  # Entrada 1 com espaçamento
 
-tk.Label(pagina2, text='CPF').pack()  # Label vazio para espaçamento
+tk.Label(pagina2, text='CPF').pack()  
 cpfADM = tk.Entry(pagina2)  # Segunda entrada
-cpfADM.pack(pady=5)  # Entrada 2 com espaçamento
+cpfADM.pack(pady=5) 
 
+tk.Label(pagina2, text='Telefone').pack() 
+telefoneADM = tk.Entry(pagina2)  # Terceira entrada
+telefoneADM.pack(pady=5) 
 
+tk.Label(pagina2, text='Horário').pack() # Caixa de seleção para Horário
+horarios = ["Manhã", "Tarde", "Noturno"]
+horarioADM = ttk.Combobox(pagina2, values=horarios, state="readonly") #state="readonly" --> Isso impede que o usuário digite valores personalizados
+horarioADM.pack(pady=5)
+horarioADM.current(0)  # Define "Manhã" como padrão
+
+tk.Label(pagina2, text='Setor').pack() 
+setorADM = tk.Entry(pagina2)  # Terceira entrada
+setorADM.pack(pady=5) 
+
+tk.Label(pagina2, text='Nível de Acesso').pack() # Caixa de seleção para Nível de Acesso
+niveis = ["Básico", "Intermediário", "Avançado"]
+nivelADM = ttk.Combobox(pagina2, values=niveis, state="readonly")
+nivelADM.pack(pady=5)
+nivelADM.current(0) 
 
 button_cadastrar = tk.Button(pagina2, text="Cadastrar", command=Cadastrar_Administrativo)
 button_cadastrar.pack(pady=20)
-
 
 button2 = tk.Button(pagina2, text="Voltar para Página 1", command=voltar_pagina1, width=20)
 button2.pack(pady=20)
@@ -117,7 +140,7 @@ button2.pack(pady=20)
 # Página 3
 pagina3 = tk.Frame(root)  # Segunda página (frame)
 
-label3 = tk.Label(pagina3, text="Esta é a Página 3", font=("Arial", 16))
+label3 = tk.Label(pagina3, text="PROFESSOR", font=("Arial", 16))
 label3.pack(pady=20)
 
 button3 = tk.Button(pagina3, text="Voltar para Página 1", command=voltar_pagina1, width=20)
